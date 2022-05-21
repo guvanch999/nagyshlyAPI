7ed7ae58f91bb8fb0f17401524b56bd82a87d537
MAIN <guvanch.d.1999@gmail.com>
2022-05-21 18:11:22 +0500

    sms app

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const Pool = require('pg').Pool;
-const Serversettings = require('../settings/dbsettings');
+const Serversettings = require('../configurations');
 
 const connectionString = `postgresql://${Serversettings.user}:${ServerSettings.password}@${ServerSettings.host}:${ServerSettings.port}/${ServerSettings.database}`;
 const pool = new Pool({
